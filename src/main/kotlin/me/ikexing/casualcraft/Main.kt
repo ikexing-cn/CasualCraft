59d9f4a6a373a4609ced66239d0a28fa61ab0fd5
ikexing-cn <ikexing@qq.com>
2022-10-29 22:15:32 +0800

    feat: init

---
+++ src/main/kotlin/me/ikexing/casualcraft/Main.kt
@@ -0,0 +1,19 @@
+package me.ikexing.casualcraft
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(
+    modid = Main.MOD_ID,
+    name = Main.MOD_NAME,
+    version = Main.MOD_VERSION,
+    modLanguageAdapter = Main.LANG_ADAPTER,
+    dependencies = Main.DEPENDENCIES
+)
+object Main {
+    const val MOD_ID = "casualcraft"
+    const val MOD_NAME = "CasualCraft"
+    const val MOD_VERSION = "1.0.0"
+
+    const val LANG_ADAPTER = "net.shadowfacts.forgelin.KotlinAdapter"
+    const val DEPENDENCIES = "required-after:crafttweaker;required-after:forgelin"
+}
